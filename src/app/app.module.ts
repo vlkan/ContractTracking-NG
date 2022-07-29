f214a2bb3e815cb6c9b49a524bb6e854920ddd27
vlkan <thkvolkanonder@gmail.com>
2022-07-29 22:05:52 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ProjectComponent } from './components/project/project.component';
+import { WorklistComponent } from './components/worklist/worklist.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    EmployeeComponent,
+    ProjectComponent,
+    WorklistComponent,
+    NotificationComponent,
+    PaymentComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/worklist/worklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worklist',
+  templateUrl: './worklist.component.html',
+  styleUrls: ['./worklist.component.css']
+})
+export class WorklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
