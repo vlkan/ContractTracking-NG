74a23826e8e26167b3887c89decca4cf8da223a6
vlkan <thkvolkanonder@gmail.com>
2022-08-05 13:23:35 +0300

    event add init

--- src/app/components/worklist-calendar/worklist-calendar.component.ts
+++ src/app/components/worklist-calendar/worklist-calendar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg, EventApi, EventClickArg } from '@fullcalendar/angular';
+import { WorkList } from 'src/app/models/worklist';
+import { WorklistService } from 'src/app/services/worklist.service';
+import { createEventId } from './worklist-utils';
 
 @Component({
   selector: 'app-worklist-calendar',
@@ -7,13 +10,78 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./worklist-calendar.component.css']
 })
 export class WorklistCalendarComponent implements OnInit {
-
-  constructor() { }
+  worklists: WorkList[] = []
+  constructor(private workListService: WorklistService) { }
 
   ngOnInit(): void {
+    this.getWorkLists()
   }
+  calendarVisible = true;
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
-  };
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'dayGridMonth',
+    initialEvents: "INITIAL_EVENTS", // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    events: [{uhuhuhuhhu:1,id: "",title: 'event 1', date: new Date}],
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  }
+  currentEvents: EventApi[] = [];
 
+  handleCalendarToggle() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  handleWeekendsToggle() {
+    const { calendarOptions } = this;
+    calendarOptions.weekends = !calendarOptions.weekends;
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+  }
+  getWorkLists() {
+    this.workListService.getWorkLists().subscribe((response) => {
+      this.worklists = response.data;
+      console.log(response);
+    });
+  }
 }
+
